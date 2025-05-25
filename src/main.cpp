8a37b1e6ef4e59d8b64b86ef423bd022648fc150
Paulo Prates <paulo.prates01@hotmail.com>
2025-05-25 17:28:53 -0300

    feat: ir strip; leitura de faixa de ir

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,44 @@
 #include <Arduino.h>
 #include <Car.hpp>
-#include <IRSensor.hpp>
+#include <IRStrip.hpp>
 #include <OTA.hpp>
 
 // denfinição da pinagem dos dispositivos de I/O
-#define LEFT_MOTOR_PIN1 33
-#define LEFT_MOTOR_PIN2 32
-#define RIGHT_MOTOR_PIN1 26
-#define RIGHT_MOTOR_PIN2 25
-#define LEFT_SENSOR_PIN 24
-#define MIDDLE_SENSOR_PIN 14
-#define RIGHT_SENSOR_PIN 12
-
-// Instanciamento dos sensores
-IRSensor leftSensor(LEFT_SENSOR_PIN, "Esquerda"); // sensor da esquerda
-IRSensor frontSensor(MIDDLE_SENSOR_PIN, "Meio"); // sensor frontal
-IRSensor rightSensor(RIGHT_SENSOR_PIN, "Direita"); // sensor da direita
+#define LEFT1 32
+#define LEFT2 33
+#define RIGHT1 25
+#define RIGHT2 26
+#define D0 8
+#define D1 9
+#define D2 10
+#define D3 11
+#define D4 12
+#define D5 13
+#define D6 15
+#define D7 17
 
 // Configuração do carro
-static car_config_t config = {
-  .left_pin1 = LEFT_MOTOR_PIN1,
-  .left_pin2 = LEFT_MOTOR_PIN2,
-  .right_pin1 = RIGHT_MOTOR_PIN1,
-  .right_pin2 = RIGHT_MOTOR_PIN2
+static car_config_t c_car = {
+  .left_pin1 = LEFT1,
+  .left_pin2 = LEFT2,
+  .right_pin1 = RIGHT1,
+  .right_pin2 = RIGHT2
 };
-
 // instanciamento do carro
-Car car(config);
+Car car(c_car);
+
+// Configuração dos sensores
+static ir_sensor_config_t c_sensor[SIZE] = {
+  {D0, "Esquerda3", -3},
+  {D1, "Esquerda2", -2},
+  {D2, "Esquerda1", -1},
+  {D3, "Centro1", 0},
+  {D4, "Centro2", 0},
+  {D5, "Direita1", 1},
+  {D6, "Direita2", 2},
+  {D7, "Direita3", 3},
+};
+IRStrip strip(c_sensor);
 
 void setup() {
   // put your setup code here, to run once:
@@ -40,4 +52,15 @@ void loop() {
   // put your main code here, to run repeatedly:
   // Atualiza o loop do OTA
   ArduinoOTA.handle();
+
+  // Controle dos sensores
+  strip.read();
+  int8_t error = strip.getError();
+  if (error < 0) {
+    car.axialRight();
+  } else if (error > 0) {
+    car.axialRight();
+  } else {
+    car.forward();
+  }
 }
